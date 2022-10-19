bc1e6cef38d026df8702b30ba56984b06a607557
pg22nicholas <pg22nicholas@vfs.com>
2022-10-19 12:34:41 -0700

    Added back button to webView stored recipe screen

--- app/src/main/java/com/vfs/recipenlpapp/views/StoredRecipeDetails.kt
+++ app/src/main/java/com/vfs/recipenlpapp/views/StoredRecipeDetails.kt
@@ -1,11 +1,14 @@
 package com.vfs.recipenlpapp.views
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NavUtils
 import androidx.core.os.bundleOf
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.vfs.recipenlpapp.R
+
 
 class StoredRecipeDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,18 @@ class StoredRecipeDetails : AppCompatActivity() {
         supportFragmentManager.commit {
             setReorderingAllowed(true)
             add<StoredRecipeWebViewFragment>(R.id.StoredRecipesFragment, args = bundle)
+        }
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.getItemId()) {
+            android.R.id.home -> {
+                onBackPressed()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
     }
 }
